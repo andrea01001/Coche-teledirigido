c9daa363e22590bc24cc5995eab9c607b10a4c50
Victor Casanova <129666003+Victorinox05@users.noreply.github.com>
2025-02-12 20:01:56 +0100

    Add files via upload
    Código para controlar la dirección del coche con un servo, a través de un mando de ps4 y una esp32

---
+++ Control_servo_mando.ino
@@ -0,0 +1,25 @@
+#include <ESP32Servo.h>
+#include <PS4Controller.h>
+
+const int pinServo = 13;
+Servo servoMotor;
+
+void setup() {
+	PS4.begin();
+	servoMotor.attach(pinServo);
+	servoMotor.write(90);
+}
+
+void loop() {
+	if (PS4.isConnected()) {//Comprueba que el mando este conectado
+		if (PS4.RStickX() <= 10 && PS4.RStickX() >= -10) { //Zona muerta del jostick (evita el drift)
+			servoMotor.write(90); //Centra el servo "Ruedas Rectas"
+		}
+		if (PS4.RStickX() >= 10 || PS4.RStickX() <= -10) {
+			int angle = map(PS4.RStickX(), 127, -127, 45, 135);
+			servoMotor.write(constrain(angle, 45, 135)); //Mueve el servo segun la posicion del jostick
+		}
+	}
+}
+/* Depende de como funciona a la hora de controlar el coche, podriamos modificar el codigo para
+ *  que el movimiento sea mas suave a la hora de girar */
